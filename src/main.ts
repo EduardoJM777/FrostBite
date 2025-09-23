104da225ab1204a77424c5cc3d9f7c3212ba50d5
EduardoJM777 <eduardo160403@edu.unipar.br>
2025-09-23 18:54:49 -0300

    a

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { CadastroUsuario } from './app/cadastro-usuario/cadastro-usuario';
 import { TelaLogin } from './app/tela-login/tela-login';
+import { Pedidos } from './app/pedidos/pedidos';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CadastroUsuario, TelaLogin],
+  imports: [CadastroUsuario, TelaLogin, Pedidos],
   template: `
     
-    <app-tela-login></app-tela-login>
-    <!-- <app-cadastro-usuario></app-cadastro-usuario> -->
+    // <app-tela-login></app-tela-login>
+    // <app-cadastro-usuario></app-cadastro-usuario>
+    <app-pedidos></app-pedidos>
 
   `,
 })
